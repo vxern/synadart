7176867265ba3cadc547dea078d1314c8023f0fd
Dorian Oszczęda <vxern@ctemplar.com>
2021-04-20 11:48:32 +0100

    Updating `ValueGenerator` to comprise static functions

--- lib/src/utils/value_generator.dart
+++ lib/src/utils/value_generator.dart
@@ -1,19 +1,19 @@
 import 'dart:math';
 
 class ValueGenerator {
-  final Random random = Random.secure();
+  static final Random random = Random.secure();
 
   /// Generates a `double` in the range [from] (inclusive) - [to] (exclusive)
-  double nextDouble({double from = 0, double to = 0}) => random.nextDouble() * (to - from) + from;
+  static double nextDouble({double from = 0, double to = 0}) => random.nextDouble() * (to - from) + from;
 
-  Iterable<double> doubleIterableSync({double from = 0, double to = 0}) sync* {
+  static Iterable<double> doubleIterableSync({double from = 0, double to = 0}) sync* {
     while (true) {
       yield nextDouble(from: from, to: to);
     }
   }
 
   /// Generates a list of size [size], filled with random double values
-  List<double> generateListWithRandomDoubles(int size, {double from = 0, double to = 0}) {
+  static List<double> generateListWithRandomDoubles(int size, {double from = 0, double to = 0}) {
     return [...doubleIterableSync(from: from, to: to).take(size)];
   }
 }
