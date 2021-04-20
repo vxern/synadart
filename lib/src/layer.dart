eba813b7674487fdd78ebe710bb859b48c925729
Dorian Oszczęda <vxern@ctemplar.com>
2021-04-20 11:49:48 +0100

    Updating documentation, adjustments and organisation of code

--- lib/src/layer.dart
+++ lib/src/layer.dart
@@ -1,38 +1,43 @@
 import 'package:neural_network/src/activation.dart';
 import 'package:neural_network/src/neuron.dart';
 
+/// Representation of single column or collection of `neurons`
 class Layer {
   late final List<Neuron> neurons;
 
   late final bool isInputLayer;
 
+  /// [activationAlgorithm] - The algorithm that should be used as the activation function of this layer's neurons
+  /// [parentLayerNeuronCount] - How many neurons the parent layer contains
+  /// [neuronCount] - How many neurons this layer should contain
   Layer({
-    required int parentNeuronCount,
-    required int neuronCount,
     required ActivationAlgorithm activationAlgorithm,
+    required int parentLayerNeuronCount,
+    required int neuronCount,
   }) {
-    isInputLayer = parentNeuronCount == 0;
+    assert(parentLayerNeuronCount >= 0, '`parentLayerNeuronCount` must not be negative');
+
+    isInputLayer = parentLayerNeuronCount == 0;
 
     neurons = List.filled(neuronCount, Neuron(
-      parentNeuronCount: parentNeuronCount,
       activationAlgorithm: activationAlgorithm,
+      parentNeuronCount: parentLayerNeuronCount,
     ));
   }
   
-  /// Accept [input] and pass the same input to each neuron
+  /// Accept [inputs] and pass the same input to each neuron
   /// 
-  /// If [isInputLayer] is true, each neutron in this layer will
-  /// receive its respective input
-  void accept(List<double> input) {
+  /// If [isInputLayer] is true, each neutron in this layer will accept an input
+  void accept(List<double> inputs) {
     if (!isInputLayer) {
       for (final neuron in neurons) {
-        neuron.inputs = input;
+        neuron.accept(inputs: inputs);
       }
       return;
     }
 
     for (int index = 0; index < neurons.length; index++) {
-      neurons[index].inputs = [input[index]];
+      neurons[index].accept(input: inputs[index]);
     }
   }
 }
