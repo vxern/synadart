26b22066a1a03cb2ba4dca90a1041709c9aa772a
Dorian Oszczęda <vxern@ctemplar.com>
2021-04-21 12:18:54 +0100

    Adding `adjust` function for use in propagation

--- lib/src/neuron.dart
+++ lib/src/neuron.dart
@@ -1,14 +1,16 @@
 import 'dart:math';
 
-import 'package:neural_network/src/activation.dart';
-import 'package:neural_network/src/utils/mathematical_operations.dart';
-import 'package:neural_network/src/utils/value_generator.dart';
+import 'package:synadart/src/activation.dart';
+import 'package:synadart/src/utils/mathematical_operations.dart';
+import 'package:synadart/src/utils/value_generator.dart';
 
 /// Representation of a single `node` in a network that can have several forms and functions. 
 /// The most basic function of the neuron is to take inputs and transform them into a single output.
 class Neuron {
   late final ActivationFunction activation;
+  late final ActivationFunction activationPrime; // The derivative of the activation function
   final List<double> weights;
+  double learningRate = 0.1; // TODO: Remove hard-coded learning rate
 
   final List<double> inputs = [];
 
@@ -19,6 +21,7 @@ class Neuron {
     assert(parentNeuronCount >= 0, '`parentNeuronCount` must not be negative');
     
     activation = resolveActivationAlgorithm(activationAlgorithm);
+    activationPrime = resolveDerivative(activationAlgorithm);
 
     if (weights.isNotEmpty || parentNeuronCount == 0) {
       return;
@@ -40,6 +43,23 @@ class Neuron {
 
     assert(input != null, 'Expected a single input but received none');
     this.inputs[0] = input!;
+  }
+  
+  /// Adjust this `Neuron`'s weights and return their adjusted values
+  List<double> adjust({required double weightMargin}) {
+    final List<double> adjustedWeights = const [];
+
+    for (int index = 0; index < weights.length; index++) {
+      adjustedWeights.add(weightMargin * weights[index]);
+      print('Current weight: ${weights[index]}');
+      weights[index] -= 
+        learningRate * 
+        -weightMargin * // TODO: Shouldn't this be δC/δa?
+        activationPrime(() => dot(inputs, weights)) * // δa/δz
+        inputs[index]; // δz/δw
+    }
+
+    return adjustedWeights;
   }
 
   /// If this neuron is an input neuron, it should output its sole input that the layer accepts.
