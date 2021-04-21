ccf7acb8e8db2adb6b717eaef3c94c647ae4d5cb
Dorian Oszczęda <vxern@ctemplar.com>
2021-04-21 12:19:28 +0100

    Added `subtract` function to subtract one list's values from another's

--- lib/src/utils/mathematical_operations.dart
+++ lib/src/utils/mathematical_operations.dart
@@ -1,7 +1,17 @@
+/// Calculates the dot product of two lists
 double dot(List<double> a, List<double> b) {
   double result = 0;
   for (int index = 0; index < a.length; index++) {
     result += a[index] * b[index];
+  }
+  return result;
+}
+
+/// Subtracts list [b] from list [a]
+List<double> subtract(List<double> a, List<double> b) {
+  List<double> result = const [];
+  for (int index = 0; index < a.length; index++) {
+    result.add(a[index] - b[index]);
   }
   return result;
 }
