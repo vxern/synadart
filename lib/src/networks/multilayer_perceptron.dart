06db0426300075dc400d3ad334587c58bdad25a9
Dorian Oszczęda <vxern@ctemplar.com>
2021-04-21 12:19:41 +0100

    Adding `multilayer_perceptron` to networks

---
+++ lib/src/networks/multilayer_perceptron.dart
@@ -0,0 +1,12 @@
+import 'package:synadart/src/activation.dart';
+import 'package:synadart/src/network.dart';
+
+class MultilayerPerceptron extends Network {
+  MultilayerPerceptron({
+    required List<int> layerSizes,
+    required ActivationAlgorithm activationAlgorithm,
+  }) : super(
+    layerSizes: layerSizes,
+    activationAlgorithm: activationAlgorithm
+  );
+}
